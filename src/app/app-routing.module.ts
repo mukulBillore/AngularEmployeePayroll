a1263f05ae9171521f02fce07a2d0b602771e7d6
abhi9971 <abhishekrajawat101gmail.com>
2022-03-09 00:32:55 +0530

    Create Employee Payroll Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"dashboard",component:DashboardComponent},
+{path:"form",component:FormComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
